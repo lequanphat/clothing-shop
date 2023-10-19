3dad39ff75b65bf1da722146a8176caebdb18932
lequanphat <lequanphat3579@gmail.com>
2023-10-19 23:02:13 +0700

    change app

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux/store';
-import './resources/css/index.css'
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
             <App />
-        </Provider>
     </React.StrictMode>,
 );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js,jsx}"],
+const plugin = require('tailwindcss/plugin');
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-      extend: {},
+        extend: {},
+        spacing: {
+            1: '8px',
+            2: '12px',
+            3: '16px',
+            4: '24px',
+            5: '32px',
+            6: '48px',
+            '35px': '35px',
+        },
+        colors: {
+            // Configure your color palette here
+            primary: '#333',
+        },
+        screens: {
+            tablet: '740px',
+
+            laptop: '1024px',
+
+            desktop: '1280px',
+        },
     },
-    plugins: [],
-}
+    plugins: [
+        plugin(function ({ addBase, theme }) {
+            addBase({
+                h1: { fontSize: '50px' },
+                h2: { fontSize: theme('fontSize.xl') },
+                h3: { fontSize: theme('fontSize.lg') },
+            });
+        }),
+    ],
+    darkMode: 'class',
+};
